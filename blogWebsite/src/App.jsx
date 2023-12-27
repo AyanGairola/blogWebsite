f450c4c683320dc5c316b4bb717e276feeb96af5
Ayan <ayan3pgairola@gmail.com>
2023-12-27 21:13:46 +0530

    first commit

---
+++ blogWebsite/src/App.jsx
@@ -0,0 +1,52 @@
+
+import { useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import './App.css'
+import {login,logout} from './store/authSlice'
+import { Footer, Header } from './components'
+
+
+
+function App() {
+  const [loading,setLoading]=useState(true);
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>{ 
+      throw error;
+    })
+    .finally(()=>{
+      setLoading(false)
+    })
+  },[])
+
+  if(loading){
+    return null;
+  }else{
+    return (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+        <div className='w-full block '>
+          <Header/>
+          <main>
+           dd {/* <Outlet/> */}
+          </main>
+          <Footer/>
+        </div>
+      </div>
+    )
+  }
+
+
+}
+
+export default App
---
+++ blogWebsite/src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from './Container/Container.jsx'
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
+import Select from './Select.jsx'
+import Login from "./Login.jsx";
+import PostForm from "./PostForm.jsx";
+import PostCard from "./PostCard.jsx";
+import AuthLayout from './AuthLayout.jsx'
+
+
+
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Select,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
+}
